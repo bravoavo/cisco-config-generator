e18c37ce4dfc4cffacd943fd45e9c2ff6c04d86e
bravoavo <hunterxxi@ya.ru>
2016-08-03 12:11:23 +0300

    Template change. Add some strings to template file. Now script contain 65 strings of code.

--- config_tempalte.js
+++ config_tempalte.js
@@ -3,7 +3,11 @@ var config_template = "\
 conf t</br>\n\
 !</br>\n\
 interface Vlan{{VLAN}}</br>\n\
+ description === LAN ===</br>\n\
+ ip nat inside</br>\n\
+ ip virtual-reassembly in</br>\n\
  ip address {{IP_ADDR}} {{MASK}}</br>\n\
+ ip tcp adjust-mss 1442</br>\n\
 exit</br>\n\
 !</br>\n\
 wr mem</br>\n\
